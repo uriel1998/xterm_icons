61b5616b68702d5e50a6d8afa0fdbcf84e6b44de
uriel1998 <steven.saus@gmail.com>
2013-01-20 18:39:01 -0500

    Updated README, added explanation to script.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ xterm_icons
 ===========
 
 A quick script to pull up a different colored icon for your xterm.  See https://gist.github.com/4582553 for a bit to add the current path to your title.
+
+Edit the script to point to the various terminal icons.
+
+Make the script executable, and set up an alias in your .bashrc (or equivalent) like this:
+
+alias xterm='xterm -e "xicons.sh;bash"'
--- xicons.sh
+++ xicons.sh
@@ -7,6 +7,8 @@
 # http://unix.stackexchange.com/questions/3197/how-to-identify-which-xterm-a-shell-or-process-is-running-in
 # http://unix.stackexchange.com/questions/16774/how-to-assign-an-icon-to-a-program-in-openbox
 
+# Remember that you'll need to set up an alias in your .bashrc and edit this script for the right path.
+
 running=$(ps aux | grep "xterm" | grep -v "grep" | grep -c "xterm")
 
 if [ "$running" -gt "7" ];then
